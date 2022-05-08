faf0d231f8bfc116c88274be2ea84105fdac9b14
Abhishek Singh <abhiwebdev2718@gmail.com>
2022-05-08 14:50:20 +0530

    cutCall

--- server.js
+++ server.js
@@ -144,11 +144,38 @@ const express=require('express'),
         console.log(e);
       }
      });
-    
+      app.post('/callCut',async (req,res)=>{
+        try {
+          const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjIiLCJ0aW1lIjoxNjM3NzQ4NTU2fQ.Bm2OqrWoAyUMfhLkdjBMW2g_1s_1wydzECl-xPUmgeM';
+          const response = await fetch(
+            "https://solvedudar.com/api-1-1/student/update_isEngage_byRoom",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                authorization: `${token}`,
+              },
+              body: JSON.stringify(req.body),
+            }
+          );
+          const data = await response.json();
+          if (data.status) {
+              res.status(200).json(data);
+          }else{
+            res.status(400).json({
+              success: false,
+              cut_call:false,
+             data
+            })
+          }
+        }catch(e) {
+          console.log(e);
+        }
+      });
       app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, './build', 'index.html'));
       });
       server.listen(port,()=>{
           console.log(`servering on ${port}`);
       });
-     // production build4
+     // production build5
